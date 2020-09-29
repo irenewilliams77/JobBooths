4d540540da490c16a187b8ee9e425b24163deff7
Irene Williams <europeandistribution@ymail.com>
2020-09-29 16:19:30 -0400

    finishing up webRTC and comments

--- public/script.js
+++ public/script.js
@@ -1,11 +1,72 @@
+//reference to socket with root path
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
+//id left undefined so peer gives us a random id
 const myPeer = new myPeer(undefined, {
     host: '/',
     port: '3001'
 })
+
+const myVideo = document.createElement('video')
+//mutes ourselves but not to other users
+myVideo.muted = true
+// allowing video/audio connection
+const peers = {}
+//connect video
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+addVideoStream(myVideo, stream)
+
+//receiving calls
+//listening to call
+myPeer.on('call', call =>{
+    call.answer(stream)
+
+    //answering call
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream)
+    addVideoStream(video, userVideoStream)
+})
+//listening to server to disconnect faster 
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+})
+//connect to new user
+socket.on('user-connected', userId =>{
+connectToNewUser(userId, stream)
+    })
+})
+
+myPeer.on('open', id =>{
+    socket.emit('join-room', ROOM_ID, id)
+})
+//send an event to the server
 socket.emit('join-room', ROOM_ID)
 
-socket.on('user-connected', userId =>{
-    console.log('user connected:' + userId)
+
+// connect to our video stream to new user/calling user and sending our video stream vice versa
+function connectToNewUser(userId, stream){
+const call = myPeer.call(userId, stream)
+const video = document.createElement('video')
+call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
 })
+//removing video after leaving
+call.on('close', () =>{
+    video.remove()
+})
+
+//connect user id to call
+peers[userId] = call
+}
+
+
+//load stream to play video/append video to screen
+function addVideoStream(video, stream) {
+    video.srcObject = streamvideo.addEventListener('loadedmetadata', () =>{
+        video.play()
+    })
+    videoGrid.append(video)
+}
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
+//set up to run the server
 const express = require('express')
 const app = express()
+// create a server with socket.io
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+// generates unique room id's
 const { v4: uuidV4} = require ('uuid')
 
+// set up for express server 
 
+//render our views using ejs
 app.set('view engine', 'ejs')
+//all of js in public folder
 app.use(express.static('public'))
-
+//taking in request and reponds
 app.get('/', (req,res) => {
+  //gives a dynamic url/random uuid
   res.redirect(`/${ uuidV4() }`)
 })
-
+//creat a route to the room/dynamic paramaters
 app.get('/:room', (req,res) => {
   res.render('room',{roomId: req.params.room})
 })
-
+//connects to the socket a user is using
 io.on('connection', socket =>{
+  //events to listen too
+  // joining a room 
   socket.on('join-room', (roomId, userId) => {
   socket.join(roomId)
   socket.to(roomId).broadcast.emit('user-connected:', userId)
+
+  //disconnect faster 
+  socket.on('disconnect', () => {
+    socket.to(roomId).broadcast.emit('user-disconnected', userId)
+  })
   })
 })
 
-
+// start up server on port 3000
 server.listen(3000)
